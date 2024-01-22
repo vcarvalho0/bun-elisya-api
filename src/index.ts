2e5f2d28d2df61f11928180a4e821b48d750f583
vcarvalho0 <vitor1.santos00@gmail.com>
2024-01-22 18:34:06 -0300

    feat: swagger and cors initial config

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,28 @@
 import { Elysia } from "elysia";
+import logger from "./logger";
+import App from "./env";
+import { cors } from "@elysiajs/cors";
+import { swagger } from "@elysiajs/swagger";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .use(
+    cors({
+      methods: ["GET", "POST", "DELETE", "PATCH", "HEAD", "OPTIONS"]
+    })
+  )
+  .use(
+    swagger({
+      documentation: {
+        info: {
+          title: "API with Elysia",
+          version: "1.0"
+        }
+      }
+    })
+  )
+  .get("/", () => "Hello Elysia")
+  .listen(App.config.port);
 
-console.log(
+logger.info(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
